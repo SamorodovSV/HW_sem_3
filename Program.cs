23c5adc9f6ed763861755ef5dbad974b7ceda3de
Samorodov Sergey <samorodovsv@yandex.ru>
2022-07-07 09:55:49 +0300

    добавили третью задачу

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@
 // else
 // {
 //     Console.Write(x +" -> " + "Число не является палиндромом" );
-    
+
 // }
 
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
@@ -33,9 +33,35 @@
 // {
 //     Console.WriteLine(i + " ^ 3" + " = " + i * i * i);
 // }
- 
+
 
 //  Напишите программу, которая принимает на вход координаты двух точек 
 // и находит расстояние между ними в 3D пространстве.
 // A(3,6,8); B(2,1,-7), -> 15.84
 // A(7,-5, 0); B(1,-1,9) -> 11.53
+
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         double CoordSystem3D(int x1, int y1, int z1, int x2, int y2, int z2)
+//         {
+//             return Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
+//         }
+
+//         Console.Write("Введите координату x1:");
+//         int x1 = Convert.ToInt32(Console.ReadLine());
+//         Console.Write("Введите координату y1:");
+//         int y1 = Convert.ToInt32(Console.ReadLine());
+//         Console.Write("Введите координату z1:");
+//         int z1 = Convert.ToInt32(Console.ReadLine());
+//         Console.Write("Введите координату x2:");
+//         int x2 = Convert.ToInt32(Console.ReadLine());
+//         Console.Write("Введите координату y2:");
+//         int y2 = Convert.ToInt32(Console.ReadLine());
+//         Console.Write("Введите координату z2:");
+//         int z2 = Convert.ToInt32(Console.ReadLine());
+
+//         Console.WriteLine("Расстояние между точками: " + CoordSystem3D(x1, y1, z1, x2, y2, z2));
+//     }
+// }
